1f3354b939dbb257ef489364f712ea344a12dbb4
Anil Yadav <anilyadav8149838058@gmail.com>
2025-04-19 18:53:33 +0530

    Finished DigitalMastery page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import DigitalMastery from './components/DigitalMastery'
 
 const App = () => {
   return (
     <div>
       <Nav/>
       <Hero/>
+      <DigitalMastery/>
     </div>
   )
 }
